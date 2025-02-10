5061048a96d50db6a34f50a24cc9bdcc41a9b478
Frank Denis <github@pureftpd.org>
2025-02-10 16:09:20 +0100

    Don't mention the soure code license in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! A small, self-contained SHA1 and HMAC-SHA1 implementation
-//! (C) Frank Denis <fdenis [at] fastly [dot] com>, public domain
+//! (C) Frank Denis <fdenis [at] fastly [dot] com>
 
 #![no_std]
 #![allow(
